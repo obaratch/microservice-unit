d479d5762c6844981ae680cf2bb841e25ad65851
Takashi Obara <takashi.obara@obalab.com>
2023-02-16 17:20:39 +0900

    add jest

---
+++ app-backend/test/empty.test.js
@@ -0,0 +1,5 @@
+describe("server empty test", () => {
+  it("should just pass", () => {
+    /* empty */
+  });
+});
---
+++ app-frontend/test/empty.test.js
@@ -0,0 +1,5 @@
+describe("front empty test", () => {
+  it("should just pass", () => {
+    /* empty */
+  });
+});
